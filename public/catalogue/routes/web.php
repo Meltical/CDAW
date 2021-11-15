4f185eeec2b462ecef6e9ad2ceea0a614d2225ed
Victor DELIEGE <victor.deliege@etu.imt-nord-europe.fr>
2021-11-15 16:21:31 +0100

    first name route + docker

--- public/catalogue/routes/web.php
+++ public/catalogue/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('name/{firstname?}/{lastname?}', function ($firstname="Victor", $lastname="Deliege") {
+    return 'Hello ' .$firstname . " " .$lastname;
 });
