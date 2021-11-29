317f866885369577e58b7e4998e2de25f10814af
Arthur <arthur.vergaert@etu.imt-nord-europe.fr>
2021-11-29 16:14:18 +0100

    added components

---
+++ public/catalogue/resources/views/components/card.blade.php
@@ -0,0 +1,5 @@
+<a class="flex flex-col group cursor-pointer" href="{{ URL::to('details') }}">
+    <img class="w-40 border-4 border-white rounded group-hover:shadow-lg" src="{{ $imageUrl }}" alt="komi san">
+    <h3 class="mt-3">{{ $title }}</h3>
+    <span class="text-sm text-gray-500">{{ $subtitle }}</span>
+</a>
