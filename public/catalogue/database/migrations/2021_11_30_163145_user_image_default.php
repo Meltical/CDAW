55936a4f3ba91b3f2ed80340a0925d12fd1e3a57
Arthur <arthur.vergaert@etu.imt-nord-europe.fr>
2021-11-30 17:34:08 +0100

    added migration for default images user

--- public/catalogue/database/migrations/2021_11_30_163145_user_image_default.php
+++ public/catalogue/database/migrations/2021_11_30_163145_user_image_default.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateUser extends Migration
+class UserImageDefault extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,8 @@ class UpdateUser extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('avatarUrl');
-            $table->string('bannerUrl');
+            $table->string('avatarUrl')->default('https://45secondes.fr/wp-content/uploads/2021/05/Y-aura-t-il-Komi-san-wa-Comyushou-Anime-Adaptation-Tout-ce-que.png')->change();
+            $table->string('bannerUrl')->default('https://furansujapon.com/wp-content/uploads/2021/05/Komi-san-wa-Komyushou-Desu.jpg')->change();
         });
     }
 
