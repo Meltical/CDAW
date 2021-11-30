bbce0f0e951f874790642d653670bd39d95a8a0e
Arthur <arthur.vergaert@etu.imt-nord-europe.fr>
2021-11-30 23:42:57 +0100

    checked if logged in navbar

--- public/catalogue/resources/views/components/navbar.blade.php
+++ public/catalogue/resources/views/components/navbar.blade.php
@@ -2,18 +2,17 @@
      <div class="flex justify-center items-center h-64 border">
          <div class="flex flex-col justify-center items-center">
              @if (Auth::user())
-                 <img class="w-20 h-20 object-cover rounded-full mb-4 " src="{{ Auth::user()->avatarUrl }}"
-                     alt="profile img">
-                 <span>{{ Auth::user()->name }}</span>
-                 <form action="{{ route('logout') }}" method="post">
-                     @csrf
-                     <button type="submit" class="text-sm underline text-gray-400 cursor-pointer">Logout</button>
-                 </form>
+             <img class="w-20 h-20 object-cover rounded-full mb-4 " src="{{ Auth::user()->avatarUrl }}" alt="profile img">
+             <span>{{ Auth::user()->name }}</span>
+             <form action="{{ route('logout') }}" method="post">
+                 @csrf
+                 <button type="submit" class="text-sm underline text-gray-400 cursor-pointer">Logout</button>
+             </form>
              @else
-                 <div class="w-20 h-20 rounded-full bg-red-300 mb-4 flex justify-center items-center">
-                     <i class="fas fa-2x fa-user text-white"></i>
-                 </div>
-                 <a href="{{ route('login') }}" class="font-bold hover:underline cursor-pointer">Login</a>
+             <div class="w-20 h-20 rounded-full bg-red-300 mb-4 flex justify-center items-center">
+                 <i class="fas fa-2x fa-user text-white"></i>
+             </div>
+             <a href="{{ route('login') }}" class="font-bold hover:underline cursor-pointer">Login</a>
              @endif
          </div>
      </div>
@@ -23,6 +22,7 @@
                  <li>
                      <a class="text-red-500 text-lg font-bold" href="{{ URL::to('/') }}">Discover</a>
                  </li>
+                 @if (Auth::user())
                  <li>
                      <a href="{{ route('history') }}">History</a>
                  </li>
@@ -32,9 +32,9 @@
                  <li>
                      <a href="{{ route('playlists') }}">Playlists</a>
                  </li>
+                 @endif
                  <li>
-                     <a class="text-sm text-gray-700 italic" href="https://github.com/MikUwU/CDAW/issues"
-                         target="_blank">Help and feedback</a>
+                     <a class="text-sm text-gray-700 italic" href="https://github.com/MikUwU/CDAW/issues" target="_blank">Help and feedback</a>
                  </li>
              </ul>
          </div>
