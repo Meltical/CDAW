ea13d3009d17ed13cfcab3c08a49cd49ffb3b760
Victor DELIEGE <victor.deliege@etu.imt-nord-europe.fr>
2021-12-04 13:47:07 +0100

    fix add button to home/playlists

--- public/catalogue/resources/views/home.blade.php
+++ public/catalogue/resources/views/home.blade.php
@@ -27,25 +27,23 @@
 
         <!-- main content-->
         <section class="h-screen overflow-y-scroll flex-grow p-10 bg-gray-100">
-            <form method="GET" action="{{ route('media.search') }}" class="flex mb-4 items-center w-3/4 ">
-                @csrf
-                <input type="text" class="w-full h-8 mr-4 rounded " name="search" placeholder="Search..." />
-                <a onclick="searchMedias(event)" class="cursor-pointer">
-                    <i class="text-gray-500 fas fa-search"></i>
-                </a>
-            </form>
 
             <div class="flex justify-between text-xl font-bold mb-6">
-                <div class="flex gap-4">
-                    <h2>{{ $title }}</h2>
-                    <button onclick="search()" class="p-1 rounded text-sm text-white bg-red-300">search</button>
-                </div>
+                <form method="GET" action="{{ route('media.search') }}" class="flex mb-4 items-center w-3/4 ">
+                    @csrf
+                    <input type="text" class="w-full h-8 mr-4 rounded " name="search" placeholder="Search..." />
+                    <a onclick="searchMedias(event)" class="cursor-pointer">
+                        <i class="text-gray-500 fas fa-search"></i>
+                    </a>
+                </form>
                 @if (Auth::user()?->isModerator())
-                    <a href="{{ route('media.create') }}">
+                    <a class="flex justify-center items-center px-3 py-2 rounded-lg bg-red-400 text-white h-10 w-10"
+                        href="{{ route('media.create') }}">
                         <i class="fas fa-plus"></i>
                     </a>
                 @endif
             </div>
+
 
             <div id="mediaContainer" class="flex flex-wrap gap-12">
                 @if (!count($medias))
@@ -59,11 +57,6 @@
             </div>
         </section>
     </div>
-    <script>
-        function search() {
-            alert('Press ctrl+f to search')
-        }
-    </script>
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
